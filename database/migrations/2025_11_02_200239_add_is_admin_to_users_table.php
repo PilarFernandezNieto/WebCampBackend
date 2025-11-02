f21a120ef455b973cd4221bde023a72556227c1f
PilarFernandezNieto <apreilmc@gmail.com>
2025-11-02 21:32:13 +0100

    middleware is admin

--- database/migrations/2025_11_02_200239_add_is_admin_to_users_table.php
+++ database/migrations/2025_11_02_200239_add_is_admin_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('isAdmin')->default(0);
+            $table->tinyInteger('is_admin')->default(0);
         });
     }
 
@@ -22,7 +22,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('isAdmin');
+            $table->dropColumn('is_admin');
         });
     }
 };
+
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\PonenteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('admin')->middleware(['auth:sanctum', 'verified', 'is_admin'])->group(function () {
+
+    Route::apiResource('/ponentes', PonenteController::class);
+
 });
 
